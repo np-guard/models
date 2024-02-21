91802024829751d03c795b59909d1b83dd0901d1
Ziv Nevo <79099626+zivnevo@users.noreply.github.com>
2024-02-21 14:45:59 +0200

    sync latest changes + more tests (#5)
    Signed-off-by: ZIV NEVO <NEVO@il.ibm.com>

--- pkg/ipblocks/ipblocks_test.go
+++ pkg/ipblocks/ipblocks_test.go
@@ -9,19 +9,17 @@ import (
 )
 
 func TestOps(t *testing.T) {
-	ipb1 := ipblocks.NewIPBlockFromCidrOrAddress("1.2.3.0/24")
+	ipb1, err := ipblocks.NewIPBlockFromCidrOrAddress("1.2.3.0/24")
+	require.Nil(t, err)
 	require.NotNil(t, ipb1)
-	ipb2 := ipblocks.NewIPBlockFromCidrOrAddress("1.2.3.4")
+	ipb2, err := ipblocks.NewIPBlockFromCidrOrAddress("1.2.3.4")
+	require.Nil(t, err)
 	require.NotNil(t, ipb2)
-	require.True(t, ipb2.IsIPAddress("1.2.3.4"))
 	require.True(t, ipb2.ContainedIn(ipb1))
 	require.False(t, ipb1.ContainedIn(ipb2))
 
 	minus := ipb1.Subtract(ipb2)
-	minusRanges := minus.ToIPRangesList()
-	require.Len(t, minusRanges, 2)
-	require.Equal(t, "1.2.3.0-1.2.3.3", minusRanges[0])
-	require.Equal(t, "1.2.3.5-1.2.3.255", minusRanges[1])
+	require.Equal(t, "1.2.3.0-1.2.3.3, 1.2.3.5-1.2.3.255", minus.ToIPRanges())
 
 	minus2, err := ipblocks.NewIPBlock(ipb1.ToCidrListString(), []string{ipb2.ToCidrListString()})
 	require.Nil(t, err)
@@ -46,50 +44,99 @@ func TestConversions(t *testing.T) {
 	cidrs := ipb1.ToCidrList()
 	require.Len(t, cidrs, 26)
 
-	ipb2 := ipblocks.NewIPBlockFromCidrList(cidrs)
+	ipb2, err := ipblocks.NewIPBlockFromCidrList(cidrs)
+	require.Nil(t, err)
 	require.Equal(t, ipb1.ToCidrListString(), ipb2.ToCidrListString())
 
 	toprint := ipb1.ListToPrint()
 	require.Len(t, toprint, 1)
 	require.Equal(t, iprange, toprint[0])
 
-	require.Equal(t, "", ipb1.ToIPAddress())
+	require.Equal(t, "", ipb1.ToIPAddressString())
 }
 
 func TestDisjointIPBlocks(t *testing.T) {
 	allIPs := ipblocks.GetCidrAll()
-	ipb := ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.4/32", "172.0.0.0/8"})
+	ipb, err := ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.4/32", "172.0.0.0/8"})
+	require.Nil(t, err)
 
 	disjointBlocks := ipblocks.DisjointIPBlocks([]*ipblocks.IPBlock{allIPs}, []*ipblocks.IPBlock{ipb})
 	require.Len(t, disjointBlocks, 5)
-	require.Equal(t, "1.2.3.4", disjointBlocks[0].ToIPAddress()) // list is sorted by ip-block size
+	require.Equal(t, "1.2.3.4", disjointBlocks[0].ToIPAddressString()) // list is sorted by ip-block size
+
+	ipb2, err := ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.0/30"})
+	require.Nil(t, err)
+	ipb3, err := ipblocks.IPBlockFromIPRangeStr("1.2.2.255-1.2.3.1")
+	require.Nil(t, err)
+	disjointBlocks = ipblocks.DisjointIPBlocks([]*ipblocks.IPBlock{ipb2}, []*ipblocks.IPBlock{ipb3})
+	require.Len(t, disjointBlocks, 3)
+	require.Equal(t, "1.2.2.255", disjointBlocks[0].ToIPAddressString())
+	require.Equal(t, "1.2.3.2/31", disjointBlocks[1].ToCidrListString())
+	require.Equal(t, "1.2.3.0/31", disjointBlocks[2].ToCidrListString())
 }
 
-func TestIsAddressInSubnet(t *testing.T) {
-	res, err := ipblocks.IsAddressInSubnet("1.2.3.4", "1.0.0.0/8")
+func TestPairCIDRsToIPBlocks(t *testing.T) {
+	first, second, err := ipblocks.PairCIDRsToIPBlocks("5.6.7.8/24", "4.9.2.1/32")
 	require.Nil(t, err)
-	require.True(t, res)
+	require.Equal(t, "5.6.7.0/24", first.ListToPrint()[0])
+	require.Equal(t, "4.9.2.1/32", second.ListToPrint()[0])
 
-	res, err = ipblocks.IsAddressInSubnet("1.2.3.4", "1.0.0.0/16")
-	require.Nil(t, err)
-	require.False(t, res)
-
-	_, err = ipblocks.IsAddressInSubnet("1.2.3.4/30", "1.0.0.0/16")
-	require.NotNil(t, err)
+	intersect := first.Intersection(second)
+	require.Equal(t, "", intersect.ToIPAddressString())
+	require.Empty(t, intersect.ListToPrint())
+	require.Empty(t, intersect.ToCidrListString())
 }
 
 func TestPrefixLength(t *testing.T) {
-	ipb := ipblocks.NewIPBlockFromCidrOrAddress("42.5.2.8/20")
+	ipb, err := ipblocks.NewIPBlockFromCidrOrAddress("42.5.2.8/20")
+	require.Nil(t, err)
 	prefLen, err := ipb.PrefixLength()
 	require.Nil(t, err)
 	require.Equal(t, int64(20), prefLen)
 
-	ipb = ipblocks.NewIPBlockFromCidrOrAddress("42.5.2.8")
+	ipb, err = ipblocks.NewIPBlockFromCidrOrAddress("42.5.2.8")
+	require.Nil(t, err)
 	prefLen, err = ipb.PrefixLength()
 	require.Nil(t, err)
 	require.Equal(t, int64(32), prefLen)
 
-	ipb = ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.4/32", "172.0.0.0/8"})
+	ipb, err = ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.4/32", "172.0.0.0/8"})
+	require.Nil(t, err)
 	_, err = ipb.PrefixLength()
+	require.NotNil(t, err)
+}
+
+func TestBadPath(t *testing.T) {
+	_, err := ipblocks.NewIPBlock("not-a-cidr", nil)
+	require.NotNil(t, err)
+
+	_, err = ipblocks.NewIPBlock("2.5.7.9/24", []string{"5.6.7.8/20", "not-a-cidr"})
+	require.NotNil(t, err)
+
+	_, err = ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.4/20", "not-a-cidr"})
+	require.NotNil(t, err)
+
+	_, err = ipblocks.NewIPBlockFromCidrList([]string{"1.2.3.4/20", "1.2.3.4/40"})
+	require.NotNil(t, err)
+
+	_, err = ipblocks.IPBlockFromIPRangeStr("1.2.3.4")
+	require.NotNil(t, err)
+
+	_, err = ipblocks.IPBlockFromIPRangeStr("prefix-1.2.3.4")
+	require.NotNil(t, err)
+
+	_, err = ipblocks.IPBlockFromIPRangeStr("1.2.3.290-1.2.3.4")
+	require.NotNil(t, err)
+
+	_, err = ipblocks.IPBlockFromIPRangeStr("1.2.3.4-suffix")
+	require.NotNil(t, err)
+
+	_, err = ipblocks.IPBlockFromIPRangeStr("1.2.3.4-2.5.6.7/20")
+	require.NotNil(t, err)
+
+	_, _, err = ipblocks.PairCIDRsToIPBlocks("1.2.3.4/40", "1.2.3.5/24")
+	require.NotNil(t, err)
+
+	_, _, err = ipblocks.PairCIDRsToIPBlocks("1.2.3.4/20", "not-a-cidr")
 	require.NotNil(t, err)
 }
