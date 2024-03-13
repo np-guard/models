021e78646153a1af4aca66106a00bfb998b01eef
Elazar Gershuni <elazarg@ibm.com>
2024-03-13 14:10:51 +0200

    protocols package
    Signed-off-by: Elazar Gershuni <elazarg@ibm.com>

---
+++ pkg/netp/common.go
@@ -0,0 +1,20 @@
+// Copyright 2020- IBM Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package netp
+
+type Protocol interface {
+	// InverseDirection returns the response expected for a request made using this protocol
+	InverseDirection() Protocol
+}
+
+type AnyProtocol struct{}
+
+func (t AnyProtocol) InverseDirection() Protocol { return AnyProtocol{} }
+
+type ProtocolString string
+
+const (
+	ProtocolStringTCP  ProtocolString = "TCP"
+	ProtocolStringUDP  ProtocolString = "UDP"
+	ProtocolStringICMP ProtocolString = "ICMP"
+)
