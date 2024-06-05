c87f0853765511ed34468193ad39d0a51d067ac0
shirim <shirim@il.ibm.com>
2024-06-05 09:04:30 +0300

    refactoring after redundant code was removed

--- pkg/connection/statefulness.go
+++ pkg/connection/statefulness.go
@@ -20,29 +20,24 @@ func PartitionTCPNonTCP(conn *Set) (tcp, nonTCP *Set) {
 
 // WithStatefulness returns the stateful part of `c`
 // `c` represents a src-to-dst connection, and `secondDirectionConn` represents dst-to-src connection.
-// This function also returns a connection object with the exact subset of the stateful part (within TCP)
-// from the entire connection `c`, and with the original connections on other protocols.
+// Returns a connection object with the exact subset of the stateful part (within TCP)
+// from the entire connection `c` and with the original connections on other protocols.
 func (c *Set) WithStatefulness(secondDirectionConn *Set) *Set {
 	connTCP := c.Intersect(NewTCPSet())
 	if connTCP.IsEmpty() {
 		return c
 	}
-	statefulCombinedConnTCP := connTCP.connTCPStatefulness(secondDirectionConn.Intersect(NewTCPSet()))
+	tcpSecondDirection := secondDirectionConn.Intersect(NewTCPSet())
+	// flip src/dst ports before intersection
+	tcpSecondDirectionFlipped := tcpSecondDirection.SwitchSrcDstPorts()
+	// tcp connection stateful subset
+	statefulCombinedConnTCP := connTCP.Intersect(tcpSecondDirectionFlipped)
 	return c.Subtract(connTCP).Union(statefulCombinedConnTCP)
 }
 
-// connTCPWithStatefulness assumes that both `c` and `secondDirectionConn` are within TCP.
-// it returns the subset from `c` which is stateful.
-func (c *Set) connTCPStatefulness(secondDirectionConn *Set) *Set {
-	// flip src/dst ports before intersection
-	statefulCombinedConn := c.Intersect(secondDirectionConn.switchSrcDstPortsOnTCP())
-	return statefulCombinedConn
-}
-
-// switchSrcDstPortsOnTCP returns a new Set object, built from the input Set object.
-// It assumes the input connection object is only within TCP protocol.
-// For TCP the src and dst ports on relevant cubes are being switched.
-func (c *Set) switchSrcDstPortsOnTCP() *Set {
+// SwitchSrcDstPorts returns a new Set object, built from the input Set object.
+// The src and dst ports on relevant cubes are being switched.
+func (c *Set) SwitchSrcDstPorts() *Set {
 	if c.IsAll() {
 		return c.Copy()
 	}
