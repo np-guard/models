e84b32f992b602346814a5d5558b8328de0116cb
adisos <adisos@il.ibm.com>
2024-08-14 15:57:07 +0300

    update tests, WIP
    Signed-off-by: adisos <adisos@il.ibm.com>

--- pkg/netset/connectionset_test.go
+++ pkg/netset/connectionset_test.go
@@ -8,17 +8,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestConnectionSet(t *testing.T) {
+func TestConnectionSetBasicOperations(t *testing.T) {
 	src1, _ := netset.IPBlockFromCidr("10.240.10.0/24")
 	dst1, _ := netset.IPBlockFromCidr("10.240.10.0/32")
 	dst2 := src1.Subtract(dst1)
 	conn1 := netset.ConnectionSetFrom(src1, dst1, connection.NewTCPSet())
 	conn2 := netset.ConnectionSetFrom(src1, dst2, connection.NewTCPSet())
-
-	unionConn := conn1.Union(conn2)
 	conn3 := netset.ConnectionSetFrom(src1, src1, connection.NewTCPSet())
 
+	// basic union & Equal test
+	unionConn := conn1.Union(conn2)
 	require.True(t, unionConn.Equal(conn3))
 	require.True(t, conn3.Equal(unionConn))
+
+	// basic subtract & Equal test
+	conn4 := netset.ConnectionSetFrom(src1, dst2, connection.All())
+	subttractionRes := conn3.Subtract(conn4)
+	require.True(t, subttractionRes.Equal(conn1))
+	require.True(t, conn1.Equal(subttractionRes))
+
+	// basic IsSubset test
+	require.True(t, conn1.IsSubset(conn3))
+	require.True(t, conn2.IsSubset(conn3))
+	require.False(t, conn2.IsSubset(conn1))
+	require.False(t, conn1.IsSubset(conn2))
+
+	// basic IsEmpty test
+	require.False(t, conn1.IsEmpty())
+	require.True(t, netset.NewConnectionSet().IsEmpty())
 
 }
