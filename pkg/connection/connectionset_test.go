e012516d93f99e7a0a9ecb2a3ff264c2467c6276
adisos <adisos@il.ibm.com>
2024-09-27 10:13:47 +0300

    update
    Signed-off-by: adisos <adisos@il.ibm.com>

--- pkg/connection/connectionset_test.go
+++ pkg/connection/connectionset_test.go
@@ -6,6 +6,7 @@ SPDX-License-Identifier: Apache-2.0
 package connection_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -35,9 +36,13 @@ func TestBasicSetICMP(t *testing.T) {
 func TestICMPTypeOutOfRange(t *testing.T) {
 	c1 := connection.ICMPConnection(135, 136, connection.MinICMPCode, connection.MaxICMPCode)
 	c2 := connection.ICMPConnection(connection.MinICMPType, connection.MaxICMPType, connection.MinICMPCode, connection.MaxICMPCode)
+	union := c1.Union(c2)
+	fmt.Println(c1.String())
+	fmt.Println(c2.String())
+	fmt.Println(union.String())
 	require.Equal(t, "protocol: ICMP icmp-type: 135-136", c1.String())
 	require.Equal(t, "protocol: ICMP", c2.String())
-	require.Equal(t, "protocol: ICMP", c1.Union(c2).String())
+	require.Equal(t, "protocol: ICMP", union.String())
 }
 
 func TestBasicSetTCP(t *testing.T) {
