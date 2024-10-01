1bc69bca555da707e5f81b2adcc08334fd966a32
adisos <adisos@il.ibm.com>
2024-10-01 09:15:40 +0300

    small update
    Signed-off-by: adisos <adisos@il.ibm.com>

--- pkg/netset/netset.go
+++ pkg/netset/netset.go
@@ -4,22 +4,11 @@ Copyright 2023- IBM Inc. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 
+// package netset implements types for network connection sets objects and operations.
+// Types defined in this package:
+// IPBlock - captures a set of IP ranges
+// TCPUDPSet - captures sets of protocols (within TCP,UDP only) and ports (source and destination)
+// ICMPSet - captures sets of types,codes for ICMP protocol
+// TransportSet - captures connection-sets for protocols from {TCP, UDP, ICMP}
+// ConnectionSet - captures a set of connections for tuples of (src IP range, dst IP range, TransportSet)
 package netset
-
-/*
-package netset implements types for network connection sets objects and operations.
-
-Types defined in this package:
---------------------------------------------------------------------------------------
-
-IPBlock - captures a set of IP ranges
-
-TCPUDPSet - captures sets of protocols (within TCP,UDP only) and ports (source and destination)
-
-ICMPSet - captures sets of types,codes for ICMP protocol
-
-TransportSet - captures connection-sets for protocols from {TCP, UDP, ICMP}
-
-ConnectionSet - captures a set of connections for tuples of (src IP range, dst IP range, TransportSet)
-
-*/
